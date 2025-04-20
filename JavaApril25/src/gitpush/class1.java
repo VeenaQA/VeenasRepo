b1425deb2aa4aec946a6a9832b07aa0a5d69db57
veena <veena@VeenaPC>
2025-04-20 17:26:40 +0530

    this is my second commit

--- JavaApril25/src/gitpush/class1.java
+++ JavaApril25/src/gitpush/class1.java
@@ -3,7 +3,7 @@ package gitpush;
 public class class1 {
 	public void m1()
 	{
-		System.out.println("this is my first method for fit push");
+		System.out.println("this is my first method for git push");
 	}
 	public static void main(String[] args) {
 		class1 obj1 =new class1();
