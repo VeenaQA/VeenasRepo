71c572974595c19c697f75a3f59ce9a57ec2f211
veena <veena@VeenaPC>
2025-04-20 17:18:50 +0530

    pushing code for the first time

---
+++ JavaApril25/src/gitpush/class1.java
@@ -0,0 +1,13 @@
+package gitpush;
+
+public class class1 {
+	public void m1()
+	{
+		System.out.println("this is my first method for fit push");
+	}
+	public static void main(String[] args) {
+		class1 obj1 =new class1();
+		obj1.m1();
+	}
+
+}
