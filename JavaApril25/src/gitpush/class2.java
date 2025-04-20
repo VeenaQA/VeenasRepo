12f20ee853aecc5b36ad47ef11a26bc8e318e2df
veena <veena@VeenaPC>
2025-04-20 17:46:53 +0530

    this is my final commit

--- JavaApril25/src/gitpush/class2.java
+++ JavaApril25/src/gitpush/class2.java
@@ -3,7 +3,7 @@ package gitpush;
 public class class2 {
 	public void method2()
 	{
-		System.out.println("this is my second method");
+		System.out.println("iam changing  my second method");
 	}
 	public static void main(String[] args) {
 		class2 obj1=new class2();
