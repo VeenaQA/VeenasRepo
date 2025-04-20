193c421dcc021b36b3ef395996e62c124a9f58c1
veena <veena@VeenaPC>
2025-04-20 17:43:19 +0530

    this is the second class am adding for commit

---
+++ JavaApril25/src/gitpush/class2.java
@@ -0,0 +1,13 @@
+package gitpush;
+
+public class class2 {
+	public void method2()
+	{
+		System.out.println("this is my second method");
+	}
+	public static void main(String[] args) {
+		class2 obj1=new class2();
+		obj1.method2();
+	}
+
+}
